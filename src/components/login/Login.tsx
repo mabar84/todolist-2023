194ce59eddb993a2cbb4520c9d061fb444027b10
mabar84 <mabar@tut.by>
2024-02-24 23:15:31 +0300

    Login refactoring, useLogin added

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -7,48 +7,14 @@ import FormGroup from "@mui/material/FormGroup";
 import FormLabel from "@mui/material/FormLabel";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { useFormik } from "formik";
-import { authSelectors, authThunks } from "reducers/authSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { authSelectors } from "reducers/authSlice";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { useLogin } from "components/login/useLogin";
 
 export const Login = () => {
   const isLoggedIn = useSelector(authSelectors.isLoggedIn);
-  const dispatch = useDispatch();
-
-  type FormikErrorType = {
-    email?: string;
-    password?: string;
-    rememberMe?: boolean;
-  };
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      rememberMe: false,
-    },
-    validate: (values) => {
-      const errors: FormikErrorType = {};
-
-      if (!values.email) {
-        errors.email = "Required";
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = "Invalid email address";
-      }
-      if (!values.password) {
-        errors.password = "Required";
-      } else if (values.password.length < 4) {
-        errors.password = "Too short";
-      }
-      return errors;
-    },
-    onSubmit: (values) => {
-      formik.resetForm();
-      dispatch(authThunks.login(values));
-
-      // alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const { formik } = useLogin();
 
   if (isLoggedIn) {
     return <Navigate to={"/"} />;
---
+++ src/components/login/useLogin.ts
@@ -0,0 +1,41 @@
+import { useDispatch } from "react-redux";
+import { useFormik } from "formik";
+import { authThunks } from "reducers/authSlice";
+
+export const useLogin = () => {
+  const dispatch = useDispatch();
+
+  type FormikErrorType = {
+    email?: string;
+    password?: string;
+    rememberMe?: boolean;
+  };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+    validate: (values) => {
+      const errors: FormikErrorType = {};
+
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.password) {
+        errors.password = "Required";
+      } else if (values.password.length < 4) {
+        errors.password = "Too short";
+      }
+      return errors;
+    },
+    onSubmit: (values) => {
+      formik.resetForm();
+      dispatch(authThunks.login(values));
+      // alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return { formik };
+};
